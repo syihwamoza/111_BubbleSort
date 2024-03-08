5c896d9a9f30b269040c953ce135bf569222a84a
syihwamoza <mozakastella26@gmail.com>
2024-03-08 13:03:44 +0700

    procedur untuk mengurutkan array dengan metode bubble sort

--- 20230140111/20230140111.cpp
+++ 20230140111/20230140111.cpp
@@ -44,5 +44,20 @@ void bubbleSortArray() {
 }
 
 
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "=================================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << a[j] << endl;
+
+	}
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
+
+
+
 
 
